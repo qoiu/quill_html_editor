96c8a66cb4c174e92a2ac874a3157c0684ee4d39
qoiu <guschinnick@gmail.com>
2025-07-29 11:59:48 +0300

    fix style

--- lib/src/widgets/webviewx/src/utils/css_loader.dart
+++ lib/src/widgets/webviewx/src/utils/css_loader.dart
@@ -23,8 +23,8 @@ class CssLoader {
       border-radius: 50%;  
       animation: loader 1s infinite ease-in-out;
     }
-    .h1 { font-size: 24px; font-weight: bold; }
-    .h2 { font-size: 20px; }
+    h1 { font-size: 24px; font-weight: bold; }
+    h2 { font-size: 20px; }
     @keyframes loader {
       0% {
       transform: scale(0);
